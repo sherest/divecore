05874bafd71ae5deba0be2acd67d6e0115d45e83
Sherest Mehta <svfranklindemo@gmail.com>
2025-05-28 17:59:33 +0530

    updated path with my local-tunnel

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -8,7 +8,7 @@
     });
 
     try {
-      const response = await fetch("http://localhost:5678/webhook/submit-form", {
+      const response = await fetch("https://calm-parts-attend.loca.lt/webhook/submit-form", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
